ca27967334e6fd33526157b8bffe10821b7ab5cc
Kittithong <koonkao.kr@gmail.com>
2026-07-24 11:08:35 +0700

    25135

---
+++ LabSheet04/TodoList.java
@@ -0,0 +1,29 @@
+
+public class TodoList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DoublyLinkedList todoList = new DoublyLinkedList();
+		
+		todoList.insert("Finish Homework");
+		System.out.println(todoList.traversal());
+		
+		todoList.insert("Laundry");
+		todoList.insert("Group Meeting");
+		System.out.println(todoList.traversal());
+		
+		todoList.insert(0, "Submit Report");
+		System.out.println(todoList.traversal());
+		
+		todoList.insert(2, "Buy Food");
+		System.out.println(todoList.traversal());
+		
+		todoList.insert("go to gym");
+		System.out.print(todoList.traversal());
+		
+		
+
+	}
+
+}
